c68058d7f1c1b8162c8236bd628d2b974f5162a0
Jose Carlos Cárdenas Martínez <josenanodev@gmail.com>
2024-02-06 13:51:56 -0600

    feat(juagadaMaquina)

--- piedraPapelOTijera.js
+++ piedraPapelOTijera.js
@@ -1,10 +1,30 @@
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.question('Piedra, papel o tijera? ', function(respuesta) {
-    console.log("Tu respuesta es: " + respuesta);
-    rl.close();
+rl.question("Piedra, papel o tijera? ", function (respuesta) {
+  console.log("Tu respuesta es: " + respuesta);
+  rl.close();
 });
+
+const jugadasPosibles = ["piedra", "papel", "tijera"];
+
+function jugadaMaquina() {
+  const numeroAleatorio = Math.floor(Math.random() * 3);
+  const jugada = jugadasPosibles[numeroAleatorio];
+  return jugada;
+}
+
+function compararJugadas(jugadaUsuario, jugadaMaquina) {
+  if (jugadaUsuario === jugadaMaquina) {
+    console.log("Empate");
+  } else if (jugadaUsuario === "papel") {
+    if (jugadaMaquina === "piedra") {
+      console.log("Ganaste");
+    } else if (jugadaMaquina === "tijera") {
+      console.log("Perdiste");
+    }
+  }
+}
